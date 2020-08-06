821694ace2e528ddd4d11165ba03ca8f7dfcceaa
Lamec Vasconcelos <53923631+lamecvasc3@users.noreply.github.com>
2020-08-06 09:30:34 -0300

    Add files via upload

---
+++ aula7/index.js
@@ -0,0 +1,12 @@
+// Não podemos criar constantes com palavras reservadas
+// Constates precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE LET.
+const primeiroNumero = 5
+const segundoNumero = 10
+const conta = primeiroNumero * segundoNumero
+console.log(conta)
