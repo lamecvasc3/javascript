5e0185ba2fe708deed1e5838aa14591fc3b69135
Lamec Vasconcelos <53923631+lamecvasc3@users.noreply.github.com>
2020-08-06 10:04:45 -0300

    Add files via upload
    Operadores, incremento, decremento e conversão de tipos de dados

---
+++ aula10/index.js
@@ -0,0 +1,31 @@
+/*
+* Aritméticos 
+* + Adição e concatenação 
+* - Subtração
+* / Divisão
+* * Multiplicação
+* ** Potenciação
+* % Resto da divisão
+* Precendência ( (), **, (*, /, %), (+, -) )
+* parseInt (inteiro), parseFloat (decimais) e Number(inteiro e decimais)
+*/
+
+const num1 = 10;
+const num2 = 3;
+
+console.log(num1 + num2)
+
+let contador = 1;
+
+contador++; //2
+contador++; //3
+contador++; //3
+contador--; //2
+
+contador *= 2;
+
+console.log(contador);
+
+const num3 = Number('15');
+
+console.log(typeof (num3))
