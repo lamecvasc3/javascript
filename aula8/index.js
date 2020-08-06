fd48b23d8eb25b40beb8641ac55d50de61157422
Lamec Vasconcelos <53923631+lamecvasc3@users.noreply.github.com>
2020-08-06 09:30:50 -0300

    Add files via upload

---
+++ aula8/index.js
@@ -0,0 +1,10 @@
+const nome = 'Lamec'
+const sobrenome = 'Vasconcelos'
+const idade = 19
+const peso = 60
+const altura = 1.8
+let imc = peso / (altura * altura)
+let anoNascimento = 2020 - idade
+console.log(`${nome} {sobrenome} tem ${idade} anos pesa ${peso} kg`)
+console.log(`Tem ${altura} e seu IMC é de ${imc}`) 
+console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento}`)
